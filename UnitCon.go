cf89be4c6850fdd3c8485a1d27428b0ab50c072a
Muzi Gumede <Muzigumede@outlook.com>
2020-02-27 11:49:19 +0200

    fixing bugs in func makeStruct()

--- UnitCon.go
+++ UnitCon.go
@@ -2,7 +2,10 @@ package main
 
 import ("fmt"
         "strconv"
-        "strings")
+        "strings"
+        "regexp"
+        //"reflect"
+        )
 
 func main(){
     var arr [3]string
@@ -10,21 +13,29 @@ func main(){
     arr[1] = "cm"
     arr[2] = "mm"
     fmt.Println(fmt.Sprint(cmConvert(arr[0],arr[2]))+"mm")
+    arrFormatedInput := separate("35cm to km")
+    conversionStruct = makeStruct(arrFormatedInput)
+
+    fmt.Println(conversionStruct.fromUnit)
 }
 
 func separate(userInput string) [3]string{
     var midIndex = strings.Index(userInput, "to")
+    
+    var inputUnit string = strings.Replace(userInput[0:midIndex]," ","",-1)
+    var outputUnit string = strings.Replace(userInput[midIndex+2:len(userInput)]," ","",-1)
+    re := regexp.MustCompile("[0-9]+")
+    inputValueArray := re.FindAllString(inputUnit,-1)
+    inputValue := inputValueArray[0]
 
-    if midIndex == 1 {
-        fmt.Println(midIndex)
-    }
+    inputUnit = strings.Replace(inputUnit,inputValue,"",-1)
 
-    var arr [3]string
+    arr := [3]string{inputValue, inputUnit, outputUnit}
     return arr
 }
 
 //convert user input string into an fixed array with [1]=number, [2]=unit, [3]=to unit
-/*func makeStruct(formatedInput [3]string) conversion{
+func makeStruct(formatedInput [3]string) conversion{
     //stuff to be done
     type conversion struct {
         value float64
@@ -35,11 +46,10 @@ func separate(userInput string) [3]string{
     conversion1 := conversion{
         value: formatedInput[0],
         fromUnit: formatedInput[1],
-        toUnit: formatedInput[2],
-        }
+        toUnit: formatedInput[2]}
 
     return conversion1
-}*/
+}
 
 func cmConvert(cm string, toUnit string) float64{
     var newUnit float64 = 0.0
