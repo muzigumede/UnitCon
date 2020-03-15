413ac1653aa77d44277189a8ac9701704ddb12c1
Muzi Gumede <Muzigumede@outlook.com>
2020-03-15 23:05:41 +0200

    minor

--- UnitCon.go
+++ UnitCon.go
@@ -80,8 +80,6 @@ func separate(conv *conversion, userInput string){
     conv.value      = floatInputValue
     conv.inputUnit  = inputUnit
     conv.outputUnit = outputUnit
-
-    
 }
 
 //handle type length conversions
